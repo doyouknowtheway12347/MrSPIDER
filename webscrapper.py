dd16c4340920afbeb1dd7b07832a8e3ff10b23a7
doyouknowtheway12347m <michael.erdstein@gmail.com>
2024-10-01 22:30:00 +1000

    ensuring that I use nodes in my webscrapping file. LOL just an excuse to commit

--- webscrapper.py
+++ webscrapper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import asyncio
 import aiohttp
+from node import Node
 
 TESTING_WEBSITES = ["https://webscraper.io/test-sites/e-commerce/allinone",
                     "https://webscraper.io/test-sites/e-commerce/static",
