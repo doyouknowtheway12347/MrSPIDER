48d39638dbb1473b0f5c5a2f4ed8a13f7cace9fd
doyouknowtheway12347m <michael.erdstein@gmail.com>
2024-10-02 14:01:09 +1000

    working on checking if the url is ok or not. Taking a break now

---
+++ src/test.py
@@ -0,0 +1,42 @@
+import regex
+
+def okURL(url):
+    if regex.match(r"^\/[^\/]", url):
+        print("relative url")
+    elif regex.match(r"[^\/=\-\.\+\?_~]#|^#", url):
+        print("anchor url ")
+    elif regex.match(r"^\/\/", url):
+        print("Protocol-relative URLs")
+    elif regex.match(r"%[0-9A-Fa-f]{2}+", url):
+        print("Encoded")
+    else:
+        print("happy")
+
+urls = ["/path/to/page.html",
+        "https://example.com/page#section?param=value",
+        "#anchor-on-current-page",
+        "//example.com/path/to/pag",
+        "https://example.com/path%20with%20spaces/file.php"]
+
+
+for url in urls:
+    print(url)
+    okURL(url)
+    print()
+
+
+
+
+# _ work with relative URLS
+            # /path/to/page.html
+        # _ Fragment-only URLs
+            # #anchor-on-current-page
+            # https://example.com/page#section?param=value
+        # _ Protocol-relative URLs
+            # //example.com/path/to/page
+        # _ Re directions 
+            # ?
+        # _ URLs with encoded characters
+            #  https://example.com/path%20with%20spaces/file.php
+ 
+        
--- src/webscrapping/urlHelperFunctions.py
+++ src/webscrapping/urlHelperFunctions.py
@@ -2,31 +2,35 @@ import urllib.parse
 import regex
 
 class Url:
+    _maxRecursionDepth =1 
     def __init__(self, unPassedUrl, parentUrl) -> None:
         self.unPassedUrl = unPassedUrl
         self.parentUrl = parentUrl
 
-        acceptableUrl = self._checkUrl()
-
-        # if acceptableUrl["not useable"]:
-        #     self.returningUrl = acceptableUrl["correction function"](self.unPassedUrl, self.parentUrl)
-        # else:
-        #     self.returningUrl = self.unPassedUrl
+        self._checkUrl()
     
-    def _checkUrl(self):
+    def _checkUrl(self, _recursionDepth=3):
         if regex.match(r"^\/[^\/]", self.unPassedUrl):
             print("relative url")
-        elif regex.mat
+        elif regex.match(r"[^\/=\-\.\+\?_~]#|^#", self.unPassedUrl):
+            print("anchor url ")
+        elif regex.match(r"^\/\/", self.unPassedUrl):
+            print("Protocol-relative URLs")
+        elif regex.match(r"%[0-9A-Fa-f]{2}+", self.unPassedUrl):
+            print("Encoded")
+        else:
+            print("happy")
+        
 
     
-url = Url("/path/to/page.html", "www.google.com")
+url = Url("#path/to/page.html", "www.google.com")
     
     
     # turns url to a useful form
         # _ check if url is useable
             # checks for
                 # relative
-                #  
+                # anchor url
         # _ work with relative URLS
             # /path/to/page.html
         # _ Fragment-only URLs
